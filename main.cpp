469c13da753e27956eb052c40667008c779a2fe9
Wrbrawner <wrb0035@auburn.edu>
2025-02-10 17:30:37 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,35 @@
+// Will Brawner
+// wrb0035
+// main
+
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    const double SWEETENER_IN_SODA = 0.001;
+    double sweetenerAmount;
+    cout << "What is the amount of artificial sweetener needed to kill a mouse:\n";
+    
+    cin >> sweetenerAmount;
+
+    double weightOfMouse;
+    cout << "What is the weight of the mouse:\n";
+
+    cin >> weightOfMouse;
+
+    double weightOfDieter;
+    cout << "What is the weight of the dieter:\n";
+
+    cin >> weightOfDieter;
+
+    double fatalAmount;
+    fatalAmount = (((sweetenerAmount / SWEETENER_IN_SODA) * weightOfMouse) * weightOfDieter);
+
+    cout << "You can drink ";
+    cout << fatalAmount;
+    cout << " diet soda without dying as a result.";
+}
